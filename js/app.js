2558a0cad60163b12610cc936ede0db3bf6cd33a
NessySweet <va.ogbuehi@gmail.com>
2015-03-03 23:41:07 -0500

    Add alert test for input box value

--- js/app.js
+++ js/app.js
@@ -12,43 +12,35 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
-  	/*--- Generate Secret Number ---*/
-  	var guessingGame = function() {
-  		var hotCold = function(num) {
-  			if(num <= num + 50)
-  				
-  		}
 
+  	$("#guessButton").click(function getNum() {
+  		var guessedNum = document.getElementById("userGuess");
 
-
-
-
-
-
-  	}
-
-
-
-
-
-  	var secretNum = function(x) {
-  		if (x === random(1, 100);) {
-  		}
-  		
-  	
+  			if (guessedNum.value != "0") {
+  				document.getElementById('feedback').innerHTML = "Correct!";
+  				//alert("You entered: " + guessedNum.value);
+  				//$( "#feedback" ).text( "Correct!" );
+  			} else {
+  				//testMultiply(guessedNum);
+  				alert("Put in bigger value please");
+  				//return hotCold(x);
+  			} //else if (isNaN(guessedNum)) {
+  				//alert(guessedNum + " is not a number value");
+  	});
 });
 
 /* 
 
 1. Create a function that starts the whole game ((Put all the code in a functiom called guessingGame))
-2. Create function hotCold with the argument num that puts Warmer or Colder in h1#feedback
+2. Create function hotCold with the argument num that puts Warmer or Colder in #feedback
 	2a. If num <=num + 50 , then append "Ice Cold"
 	2b. Else if num <= num + 30, then append "Cold"
 	2c. Else if num <= num + 20, then append "Warm"
 	2d. Else if num <= num + 10, then append "Hot"
 	2e. Else if num <= num + 1, then append "Very Hot"
-3. Create a function where x (the input number) is compared to a random number.
+3. Create a function where x (which will eventually be the input number) is compared to a random number.
 	2a. If they are equal, put "Correct!" in #feedback.
 	2b. Else if, run function hotCold  
-
+4. Establish a variable for the input number in #userGuess called guessedNUm
+5. ** Push an array of guessed values to the "guessed" box
 */
