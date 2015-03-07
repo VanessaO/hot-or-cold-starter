54f925b658789120894567a6b7eb6b2e2ce9d293
NessySweet <va.ogbuehi@gmail.com>
2015-03-06 23:20:30 -0500

    Alert a consistent secret number

--- js/app.js
+++ js/app.js
@@ -15,44 +15,18 @@ $(document).ready(function(){
   	var secretNum = (function() {
 			var randomNum = Math.floor(Math.random() * 101);
 			return randomNum;
-	})(); // TURNED INTO AN IIFE TO GENERATRE SECRET NUMBER ONCE
+	})(); // AN IMMEDIATELY INVOKED FUNCTION EXPRESSION (IIFE) TO GENERATRE SECRET NUMBER ONCE
 
   	/*--- Resets game with secret number for user to guess---*/
 	$(".new").click(function newGame() {
-		alert(secretNum);
+		alert(secretNum); // ALERTS A CONSISTANT SECRET NUMBER
+		//return(secretNum); // CAN THIS BE A GLOBAL VARIABLE DUE TO SCOPE?
 	});
 
 	/*--- A function that compares a value to secret number ---*/
 	function compareNum(input) {
 
-		alert(input + " is the input value. " + secretNum + " is the secret number.");
-		/*if (input == secretNum) {
-			//document.getElementById("feedback").innerHTML = "Correct!";
-			alert("Correct");
-		} 
-		else if(input <= secretNum + 1) {
-			//document.getElementById("feedback").innerHTML = "Very Hot!";
-			alert("Very Hot");
-		}
-		else if(input <= secretNum+ 10) {
-			//document.getElementById("feedback").innerHTML = "Hot!";
-			alert("Hot");
-		}
-		else if(input <= secretNum + 20) {
-			//document.getElementById("feedback").innerHTML = "Warm!";
-			alert("Wram");
-		}
-		else if(input <= secretNum + 30) {
-			//document.getElementById("feedback").innerHTML = "Cold!";
-			alert("Cold");
-		}
-		else if(input <= secretNum + 50) {
-			//document.getElementById("feedback").innerHTML = "Ice Cold!";
-			alert("Ice Cold");
-		}
-		else {
-			alert("Please enter a numeric value.");
-  		}*/
+		alert(input + " is the input value. " + /* secretNum */ + " is the secret number.");
   	}
 
   	$("#guessButton").click(function makeGuess() {
