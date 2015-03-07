2b587e6c449fcdcdd82579ce7392f62e0b248421
NessySweet <va.ogbuehi@gmail.com>
2015-03-07 13:34:57 -0500

    Add guess counter

--- js/app.js
+++ js/app.js
@@ -15,22 +15,32 @@ $(document).ready(function(){
   	var secretNum = (function() {
 			var randomNum = Math.floor(Math.random() * 101);
 			return randomNum;
-	})(); // AN IMMEDIATELY INVOKED FUNCTION EXPRESSION (IIFE) TO GENERATRE SECRET NUMBER ONCE
+	})(); // AN IMMEDIATELY INVOKED FUNCTION EXPRESSION (IIFE) TO GENERATE SECRET NUMBER ONCE
 
   	/*--- Resets game with secret number for user to guess---*/
 	$(".new").click(function newGame() {
 		alert(secretNum); // ALERTS A CONSISTANT SECRET NUMBER
-		//return(secretNum); // CAN THIS BE A GLOBAL VARIABLE DUE TO SCOPE?
+		//return(secretNum); // CAN THIS EVER BE A GLOBAL VARIABLE DUE TO SCOPE?
 	});
 
 	/*--- A function that compares a value to secret number ---*/
 	function compareNum(input) {
 
-		alert(input + " is the input value. " + /* secretNum */ + " is the secret number.");
+		alert(input + " is the input value. " + secretNum + " is the secret number.");
   	}
+  		
+  	var clicks = 0;
 
   	$("#guessButton").click(function makeGuess() {
-  		var guessedNum = parseInt(document.getElementById("userGuess").value, 10);
+  		var guessedNum = parseInt(document.getElementById("userGuess").value);  		
   		compareNum(guessedNum);
+  		if (guessedNum !== 0) {
+	  		clicks += 1;
+	        document.getElementById("count").innerHTML = clicks;
+    	}
+    	else {
+    		alert("Enter number greater than zero, please!");
+    	}
+  		return false;
   	});
 });
